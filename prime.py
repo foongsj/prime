6c76895ceb20af82bef28cbc54fa66a06319f2ed
Sui Jin Foong <foongsj@hotmail.com>
2020-11-28 17:45:31 +0800

    Removed debugs

--- prime.py
+++ prime.py
@@ -5,15 +5,19 @@ import argparse, math
 parser = argparse.ArgumentParser()
 
 parser.add_argument("number", nargs="?", type=int, default=12,
-  help="Number to factorize into primes, default 12")
+  help="Positive integer to factorize into primes, default 12")
 
 args = parser.parse_args()
 
 n = int(args.number)
+if n < 2:
+  print("There are no primes as number is less than 2.")
+  exit(1)
 limit = int(math.sqrt(n))
 
-print(n, limit)
 factors = []
+
+# Initialize with first prime
 i = 2
 while i <= limit:
   if n % i:
@@ -24,4 +28,4 @@ while i <= limit:
     limit = int(math.sqrt(n))
 
 factors.append(int(n))
-print(factors)
+print("The primes are:", factors)
